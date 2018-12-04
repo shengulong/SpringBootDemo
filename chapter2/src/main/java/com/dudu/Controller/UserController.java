53bf3114df13683403b0eb9f804c6682241e8bf7
gulong <shengulong@126.com>
2018-12-04 19:42:54 +0800

    修改为spring-boot2.0；增加controller页面;使用lombok；解决中文乱码；

--- chapter2/src/main/java/com/dudu/Controller/UserController.java
+++ chapter2/src/main/java/com/dudu/Controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * Created by tengj on 2017/2/27.
@@ -18,21 +19,44 @@ public class UserController {
     @Value("${com.dudu.want}")
     private  String want;
 
-    @Autowired
+    // @Resource默认是按照名称装配的
+    @Resource
     ConfigBean configBean;
 
+    // @Autowired是默认按照类型装配的
     @Autowired
     ConfigTestBean configTestBean;
-
 
     @Value("${com.dudu.yearhope}")
     private String yearhope;
 
 
     @RequestMapping("/")
-    public String hexo(){
-        //return configBean.getName()+configBean.getWant();
+    public String hello(){
+        return name+","+want;
+        // return configBean.getName()+configBean.getWant();
         //return yearhope;
-        return configBean.getName()+configBean.getWant();
+        // return configBean.getName()+configBean.getWant();
+    }
+
+    @RequestMapping("/test1")
+    public String test1(){
+        // return name+","+want;
+        return "test1:" + configBean.getName() + configBean.getWant();
+        //return yearhope;
+        // return configBean.getName()+configBean.getWant();
+    }
+
+    @RequestMapping("/test2")
+    public String test2(){
+        // return name+","+want;
+        // return "test:" + configBean.getName() + configBean.getWant();
+        //return yearhope;
+        return "test2:" + configTestBean.getName() + configTestBean.getWant();
+    }
+
+    @RequestMapping("/yearHope")
+    public String year() {
+        return yearhope;
     }
 }
--- chapter2/src/main/java/com/dudu/domain/ConfigBean.java
+++ chapter2/src/main/java/com/dudu/domain/ConfigBean.java
@@ -8,24 +8,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 // 通过@ConfigurationProperties加载properties文件内的配置，通过prefix属性指定properties的配置的前缀，通过locations指定properties文件的位置
 
 
+@lombok.Data
 @ConfigurationProperties(prefix = "com.dudu")
 public class ConfigBean {
     private String name;
     private String want;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getWant() {
-        return want;
-    }
-
-    public void setWant(String want) {
-        this.want = want;
-    }
 }
