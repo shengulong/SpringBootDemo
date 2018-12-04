dd3432287f538b5057535cdaba58c13444ec5a5b
gulong <shengulong@126.com>
2018-12-04 17:00:57 +0800

    修改为spring-boot2.0；增加controller页面

--- chapter1/src/main/java/com/dudu/Chapter1Application.java
+++ chapter1/src/main/java/com/dudu/Chapter1Application.java
@@ -2,6 +2,7 @@ package com.dudu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class Chapter1Application {
 	@RequestMapping("/")
 	public String index(){
 		return "Hello Spring Boot";
+	}
+
+	@GetMapping("/about")
+	public String about() {
+		return "About the application";
 	}
 
 	public static void main(String[] args) {
